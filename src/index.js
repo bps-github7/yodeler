379dd6938a8ddfba1e193281f418ded632c247cb
Ben Sehnert <bps53@protonmail.com>
2020-10-23 10:28:44 -0400

    Refactored Bootstrap Nav bar to a seperate component.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import Counter from './components/counter';
+import bootstrap_nav from './components/bootstrap_nav';
 import App from './app.js'
 
 const element = <App/>
